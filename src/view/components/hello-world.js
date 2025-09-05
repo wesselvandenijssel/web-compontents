08d2f47a855d9330a8d84f14db5e79007dc8a8b3
wesselvandenijssel <wessel@mbeffect.nl>
2025-09-05 10:19:18 +0200

    feat: enhance HelloWorld component with interactivity

--- src/view/components/hello-world.js
+++ src/view/components/hello-world.js
@@ -1,7 +1,24 @@
 export class HelloWorld extends HTMLElement {
   constructor() {
     super()
-    this.textContent = 'Hello World'
+
+    const container = document.createElement('div')
+
+    this.messageElement = document.createElement('p')
+    this.messageElement.textContent = 'Hello World'
+
+    const button = document.createElement('button')
+    button.textContent = 'Change Message'
+    button.addEventListener('click', () => this.changeMessage())
+
+    container.appendChild(this.messageElement)
+    container.appendChild(button)
+
+    this.appendChild(container)
+  }
+
+  changeMessage() {
+    this.messageElement.textContent = 'Hello Universe!'
   }
 }
 
